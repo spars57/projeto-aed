39e1034cf36f04d7241d650340dcad237e1daeb7
Alexis-Silvery <119908477+Alexis-Silvery@users.noreply.github.com>
2023-05-06 02:50:14 +0100

    changes

--- view/View.py
+++ view/View.py
@@ -15,12 +15,6 @@ class Frame(tk.Tk):
         self._frame = new_frame
         self._frame.pack()
 
-    def registar(self, user, password, nif):
-        UserController.create_user(self.creauser, self.creapassword, self.creanif)
-
-    def login(self, user, password):
-        UserController.login(self.user, self.password)
-
         
 class MainFrame(tk.Frame):
     def __init__(self, master):
@@ -32,19 +26,25 @@ class MainFrame(tk.Frame):
 
         self.welc = tk.Label(self, text="Bem vindo ao gestor de despesas").grid(row=0, column=1)
         
-        self.user1 = tk.Label(self, text="Username:").grid(row=1, column=0)
-        self.user = tk.Entry(self)
-        self.user.get()
-        self.user.grid(row=1, column=1)
+        self.user_label = tk.Label(self, text="Username:").grid(row=1, column=0)
+        self.user_entry = tk.Entry(self)
+        self.user_entry.get()
+        self.user_entry.grid(row=1, column=1)
 
-        self.password1 = tk.Label(self, text="Password:").grid(row=2, column=0)
-        self.password = tk.Entry(self, show="*")
-        self.password.get()
-        self.password.grid(row=2, column=1)
+        self.password_label = tk.Label(self, text="Password:").grid(row=2, column=0)
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.get()
+        self.password_entry.grid(row=2, column=1)
 
-        self.iniciar = tk.Button(self, text="Iniciar Sessão", command=(lambda: master.switch_frame(SessionFrame) if (master.login) else showerror('Error', mensagem))).grid(row=3, column=1)
+        self.login_button = tk.Button(self, text="Iniciar Sessão", command=(lambda: master.switch_frame(SessionFrame) if (self.login==True) else showerror('Error', mensagem))).grid(row=3, column=1)
 
-        self.criar1 = tk.Button(self, text="Criar Utilizador", command=lambda: master.switch_frame(RegisterFrame)).grid(row=4, column=1)
+        self.create_button = tk.Button(self, text="Criar Utilizador", command=lambda: self.master.switch_frame(RegisterFrame)).grid(row=4, column=1)
+
+        self.exit = tk.Button(self, text="Sair", command=lambda: self.master.destroy()).grid(row=4, column=0)
+
+    def login(self, user, password):
+        if UserController.login(self.user, self.password) == True:
+            return True
 
 class RegisterFrame(tk.Frame):
     def __init__(self,master):
@@ -55,28 +55,31 @@ class RegisterFrame(tk.Frame):
         mensagem2 = 'Utilizador Registado com Sucesso'
         mensagem3 = 'Erro na criação do user'
 
-        self.creauser1 = tk.Label(self, text="Username:").grid(row=0, column=0)
-        self.creauser = tk.Entry(self)
-        self.creauser.get()
-        self.creauser.grid(row=0, column=1)
+        self.create_user_label = tk.Label(self, text="Username:").grid(row=0, column=0)
+        self.create_user_entry = tk.Entry(self)
+        self.create_user_entry.grid(row=0, column=1)
 
-        self.creanif1 = tk.Label(self, text="NIF:").grid(row=1, column=0)
-        self.creanif = tk.Entry(self)
-        self.creanif.get()
-        self.creanif.grid(row=1, column=1)
+        self.create_nif_label = tk.Label(self, text="NIF:").grid(row=1, column=0)
+        self.create_nif_entry = tk.Entry(self)
+        self.create_nif_entry.grid(row=1, column=1)
 
-        self.creapassword1 = tk.Label(self, text="Password:").grid(row=2, column=0)
-        self.creapassword = tk.Entry(self, show="*")
-        self.creapassword.get()
-        self.creapassword.grid(row=2, column=1)
+        self.create_password_label = tk.Label(self, text="Password:").grid(row=2, column=0)
+        self.create_password_entry = tk.Entry(self, show="*")
+        self.create_password_entry.grid(row=2, column=1)
 
-        self.crearep_pass1 = tk.Label(self, text="Repeat Password:").grid(row=3, column=0)
-        self.crearep_pass = tk.Entry(self, show="*").grid(row=3, column=1)
+        self.rep_password_label = tk.Label(self, text="Repeat Password:").grid(row=3, column=0)
+        self.rep_password_entry = tk.Entry(self, show="*").grid(row=3, column=1)
 
-        self.regis = tk.Button(self, text="Registrar Utilizador", command=(lambda: showerror('Sucesso', mensagem2) and master.switch_frame(MainFrame) if master.registar else showerror('Erro', mensagem3)))
-        self.regis.grid(row=4, column=1)
+        self.register_button = tk.Button(self, text="Registrar Utilizador", command=(lambda: showerror('Sucesso', mensagem2) and master.switch_frame(MainFrame)
+                            if (self.registar(self.creauser.get(), self.creapassword.get(), self.creanif.get())==True) else showerror('Erro', mensagem3)))
+        self.register_button.grid(row=4, column=1)
 
-        self.retroceder = tk.Button(self, text="Voltar", command=lambda: master.switch_frame(MainFrame)).grid(row=4, column=0)
+        self.voltar_button = tk.Button(self, text="Voltar", command=lambda: master.switch_frame(MainFrame)).grid(row=4, column=0)
+
+    def registar(self, user, password, nif):
+        if UserController.create_user(user, password, nif):
+            return True
+        return False
 
 class SessionFrame(tk.Frame):
     def __init__(self, master):
